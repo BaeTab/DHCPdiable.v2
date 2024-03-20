363318c96751887e1f8e2dda4ca374772dfd58f6
BaeTab <bhwoo48@gmail.com>
2024-03-20 18:07:18 +0900

    - APIPA Disable , DHCP Client Service Disable

---
+++ DHCPdisable.v2/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DHCPdisable.v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("휴먼모음T", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(85, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "재부팅";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(286, 45);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 149);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(326, 188);
+            this.Name = "Form1";
+            this.Text = "DHCP Disable";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DHCPdisable.v2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace DHCPdisable.v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            eventHandler();
+            currentStatus();
+        }
+
+        private void eventHandler()
+        {
+            button3.Click += Button3_Click; // 재부팅
+        }
+
+        private void currentStatus()
+        {
+            try
+            {
+                // 레지스트리 키가 없을 경우 CreateSubKey 를 사용해야한다
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"))
+                {
+                    var value = key.GetValue("IPAutoconfigurationEnabled");
+                    // IPAutoconfigurationEnabled 키값이 이미 있는경우
+                    if (null != value)
+                    {
+                        // IPAutoconfigurationEnabled 키값이 0인경우는 이미 비활성화 이다
+                        if (0 == (int)value)
+                        {
+                            textBox1.AppendText("*APIPA 설정이 비활성화 되어 있습니다\r\n");
+                        }
+                        // 키값의 설정이 다른경우 설정해준다
+                        else
+                        {
+                            key.SetValue("IPAutoconfigurationEnabled", 0, RegistryValueKind.DWord);
+                            textBox1.AppendText("*APIPA 설정을 비활성화 했습니다\r\n");
+                        }
+                    }
+                    // IPAutoconfigurationEnabled 키값이 없는 경우 0의 값으로 생성한다
+                    else
+                    {
+                        key.SetValue("IPAutoconfigurationEnabled", 0, RegistryValueKind.DWord);
+                        textBox1.AppendText("*APIPA 설정을 비활성화 했습니다\r\n");
+                    }
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Services\Dhcp"))
+                {
+                    var value = key.GetValue("Start");
+                    if (null != value)
+                    {
+                        if (4 == (int)value)
+                        {
+                            textBox1.AppendText("*DHCP Client 서비스는 비활성화 되어 있습니다");
+                        }
+                        else
+                        {
+                            key.SetValue("Start", 4, RegistryValueKind.DWord);
+                            textBox1.AppendText("*DHCP Client 서비스를 비활성화 했습니다\r\n*재부팅이 필요합니다");
+                            button3.Enabled = true;
+                        }
+                    }
+                    else
+                    {
+                        key.SetValue("Start", 4, RegistryValueKind.DWord);
+                        textBox1.AppendText("*DHCP Client 서비스를 비활성화 했습니다\r\n*재부팅이 필요합니다");
+                        button3.Enabled = true;
+                    }
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DialogResult.Yes == MessageBox.Show("재부팅 하시겠습니까?\r\n'예(Y)'를 클릭하면 즉시 재부팅됩니다", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Information))
+                {
+                    Process.Start("shutdown.exe", "/r /t 0");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류: {ex.Message}");
+            }
+        }
+    }
+}
